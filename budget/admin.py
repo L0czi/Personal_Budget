f5ab06f6d15fe9b4a48c06b751946e5ee4436e4b
L0czi <m.tesarski55@gmail.com>
2021-08-15 14:12:12 +0200

    Update for admin page

--- budget/admin.py
+++ budget/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from . models import ExpenceWay, Expence, ExpenceCategory, Income, IncomeCategory
 
-admin.site.register(ExpenceWay)
-admin.site.register(Expence)
-admin.site.register(ExpenceCategory)
-admin.site.register(IncomeCategory)
-admin.site.register(Income)
+#admin.site.register(ExpenceWay)
+#admin.site.register(Expence)
+#admin.site.register(ExpenceCategory)
+#admin.site.register(IncomeCategory)
+#admin.site.register(Income)
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('user','income_category','ammount','date','notes')
+    list_filter = ('user', 'date')
+
+@admin.register(IncomeCategory)
+class IncomeCategoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    list_filter = ('user',)
+
+@admin.register(Expence)
+class ExpenceAdmin(admin.ModelAdmin):
+    list_display = ('user','expence_category','expence_way','ammount','date','notes')
+    list_filter = ('user', 'date')
+
+@admin.register(ExpenceCategory)
+class ExpenceCategoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    list_filter = ('user',)
+
+@admin.register(ExpenceWay)
+class ExpenceWayAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    list_filter = ('user',)
