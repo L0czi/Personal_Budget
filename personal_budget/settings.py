06827abc26554abe898539a792621dd6f72576f2
L0czi <m.tesarski55@gmail.com>
2021-09-07 12:03:11 +0200

    DEBUG set as env variable

--- personal_budget/settings.py
+++ personal_budget/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['budgetdjangoapp.herokuapp.com']
 
